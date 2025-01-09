8ae8d686277edfc7ed3619838e96e83fc3be6c79
PracheetaGupta <pracheeta9430@gmail.com>
2025-01-09 10:42:47 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ import sys
 import os
 from flask import Flask, render_template, request, jsonify
 
+
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from utils.data_preprocessing import load_data, preprocess_nutrition_data
@@ -11,24 +12,30 @@ from app.chatgroq_integration import chat_with_groq
 
 app = Flask(__name__)
 
+
 income_data = load_data("data/PerCapita.csv")
 nutrition_data = preprocess_nutrition_data(load_data("data/NutritionDatabase.csv"))
+
 
 conversation_history = [{"role": "system", "content": "You are a healthcare chatbot providing personalized nutritional advice."}]
 
 @app.route('/')
 def index():
     return render_template('index.html')  
+
 @app.route('/send', methods=['POST'])
 def send_message():
     user_input = request.json['message']
     state = request.json['state']  
     year = "2023"  
 
+    
     user_category = analyze_economic_background_kmeans(state, income_data, year)
 
+   
     prompt = generate_prompt(user_category, nutrition_data)
 
+    
     context = (
         f"You are a healthcare chatbot providing personalized nutritional advice based on economic status. "
         f"The user is categorized as: {user_category}. "
@@ -41,9 +48,12 @@ def send_message():
         f"Answer like a chatbot short and crisp, make the chatbot an interactive experience use emojis and stuff. Add humour too."
     )
 
+    
     conversation_history.append({"role": "user", "content": user_input})
 
+    
     response = chat_with_groq(user_input, conversation_history)
+    
     
     conversation_history.append({"role": "assistant", "content": response})
 
